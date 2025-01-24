c16ce7f6195b12cca784769c6db2248b6876ed3a
alanleew <alanleew412@gmail.com>
2025-01-24 01:25:59 -0800

    Completed Pixela API project

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+from datetime import datetime
 
 TOKEN = "asdfghjkl"
 USERNAME = "alanleew"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -12,16 +14,47 @@ user_params = {
     "notMinor": "yes",
 }
 
-# Code below is for creating an account. If you run again, account is already created!
+# Create account
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Running Graph",
     "unit": "mi",
     "type": "float",
     "color": "sora",
 }
-requests.post(url=graph_endpoint,)
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# Create graph
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+# https://pixe.la/v1/users/alanleew/graphs/graph1.html
+
+today = datetime(year=2025, month=1, day=1).strftime("%Y%m%d")
+
+# pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+# pixel_config = {
+#     "date": today,
+#     "quantity": "10",
+# }
+
+# POST
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# print(response.text)
+
+# UPDATE
+# update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today}"
+# update_pixel_config = {
+#     "quantity": "0",
+# }
+# response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+# print(response.text)
+
+# response = requests.delete(url=update_pixel_endpoint, headers=headers)
+# print(response.text)
